9c192af58c41d19890eccaa4a748c91843851bce
time2fishman <adam_h_martinez@hotmail.com>
2023-08-28 11:26:54 -0600

    comment out payOnline.js

--- assets/js/payOnline.js
+++ assets/js/payOnline.js
@@ -1,12 +1,12 @@
-const paypalBtns = document.querySelector('#payment_options')
-const name = document.querySelector('#name')
-const amount = document.querySelector('#amount')
-const note = document.querySelector('#note')
+// const paypalBtns = document.querySelector('#payment_options')
+// const name = document.querySelector('#name')
+// const amount = document.querySelector('#amount')
+// const note = document.querySelector('#note')
 
-paypalBtns.addEventListener('mouseover', () => {
-  console.log('you moused over a paypal button')
-  name.value = ''
-  amount.value=''
-  note.value=''
-  console.log('input fields should have cleared')
-})
+// paypalBtns.addEventListener('mouseover', () => {
+//   console.log('you moused over a paypal button')
+//   name.value = ''
+//   amount.value=''
+//   note.value=''
+//   console.log('input fields should have cleared')
+// })
